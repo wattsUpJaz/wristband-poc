b5d783df8168d1a9c810ba221fb8383300b5a289
Jazzy <15367315+wattsUpJaz@users.noreply.github.com>
2023-05-06 22:23:19 -0600

    Initial Commit

---
+++ WristbandPoc Watch App/WristbandPocApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristbandPocApp.swift
+//  WristbandPoc Watch App
+//
+//  Created by Jasmyne Carson on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WristbandPoc_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
